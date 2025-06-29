0cf6a6f1f3ddab8f9271ae09c8a3c4f8901e73f2
Harsh <gargharsh750@gmail.com>
2025-06-29 21:02:08 +0530

    add vercel configs in server

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const port=process.env.PORT || 4000;
 await connectDB()
 await connectCloudinary()
 
-const allowedOrigins=['http://localhost:5173']
+const allowedOrigins=['http://localhost:5173, https://instadelivery.vercel.app']
 //middleware configurations
 
 app.use(express.json());            
